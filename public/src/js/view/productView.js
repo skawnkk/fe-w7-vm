16873fac90d24a280bde97fc6eadc503bc8250e7
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-03-24 22:53:26 +0900

    Feat. product 구매가능 체크

--- public/src/js/view/productView.js
+++ public/src/js/view/productView.js
@@ -7,9 +7,13 @@ class ProductView {
     this.vendingModel = vendingModel;
     this.vendingMenuArea = _.$('.products');
     this.init();
+    this.vendingMenuItems = _.$All('.product-item');
   }
   init() {
     this.render();
+    this.walletModel.subscribe(this.changeChoiceable.bind(this));
+    const returnObserver = this.vendingModel.getReturnObserver();
+    this.vendingModel.subscribe(returnObserver, this.changeChoiceable.bind(this));
   }
   render() {
     this.renderProducts();
@@ -21,6 +25,14 @@ class ProductView {
       ''
     );
     this.vendingMenuArea.innerHTML = productHTML;
+  }
+  changeChoiceable() {
+    this.vendingMenuItems.forEach((item) => {
+      const price = parseInt(_.$('.product-item__price', item).innerHTML);
+      const vendingMoney = this.vendingModel.getVendingMoney();
+      if (vendingMoney >= price) item.classList.add('choiceable');
+      else item.classList.remove('choiceable');
+    });
   }
 }
 
