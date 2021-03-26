c4902f872ca6d5ee92b6398174e43d4fdb5c5406
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-03-26 12:32:32 +0900

    Refactor. log model,view 분리

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,11 +5,15 @@ import ProductView from './view/productView';
 import WalletView from './view/walletView';
 import ProcessView from './view/processView';
 import { myMoney, beverage } from './dataSetting';
+import LogModel from './model/logModel';
+import LogView from './view/logView';
 
 const walletModel = new WalletModel(myMoney);
 const productModel = new ProductModel(beverage);
 const processModel = new ProcessModel();
+const logModel = new LogModel();
 
-const processView = new ProcessView({ walletModel, processModel, productModel });
-const productView = new ProductView({ walletModel, processModel, productModel });
-const walletView = new WalletView({ walletModel, processModel });
+const processView = new ProcessView({ walletModel, processModel, productModel, logModel });
+const productView = new ProductView({ walletModel, processModel, productModel, logModel });
+const walletView = new WalletView({ walletModel, processModel, logModel });
+const logView = new LogView({ logModel });
--- public/src/js/view/processView.js
+++ public/src/js/view/processView.js
@@ -3,12 +3,12 @@ import WalletModel from '../model/walletModel';
 import { _ } from '../util/util.js';
 
 class ProcessView {
-  constructor({ walletModel, productModel, processModel }) {
+  constructor({ walletModel, productModel, processModel, logModel }) {
     this.walletModel = walletModel;
     this.productModel = productModel;
     this.processModel = processModel;
+    this.logModel = logModel;
     this.processMoneyArea = _.$('.process-money');
-    this.processStatusArea = _.$('.process-status');
     this.processReturnBtn = _.$('.process-btn');
     this.init();
   }
@@ -20,24 +20,16 @@ class ProcessView {
   addEvent() {
     _.addEvent(this.processReturnBtn, 'click', this.handleClick.bind(this));
   }
-  render() {
-    this.renderVendingMoney();
-    this.renderVendingStatus();
-  }
   handleClick() {
     const money = this.processModel.getVendingMoney();
     this.walletModel.setReturnMoneyBack(money); //wallet에 돈 ++
-    this.processModel.processClickFn(money); //자판기 돈 -- & 로그 출력
+    this.processModel.updateVendingMoney({ money, plus: false }); //자판기 돈 --
+    this.logModel.setLog({ value: money, type: 'moneyReturn' });
   }
-  renderVendingMoney() {
+  render() {
     const vendingMoney = this.processModel.getVendingMoney();
     const monitorMoneyHTML = getMonitorMoneyHTML(vendingMoney);
     this.processMoneyArea.innerHTML = monitorMoneyHTML;
-  }
-  renderVendingStatus() {
-    const vendingStatus = this.processModel.getVendingStatus();
-    const statusHTML = vendingStatus.reduce((acc, curr) => acc + getMonitorStatusHTML(curr), '');
-    this.processStatusArea.innerHTML = statusHTML;
   }
 }
 
